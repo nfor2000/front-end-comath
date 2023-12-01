1d56a03e2231adb0a5746923dd757985b2428d22
nfor2000 <nforpresly2@gmail.com>
2023-12-01 07:34:18 +0100

    commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+     <footer className="footer">
+          &copy; copyright @2023 by <span>group 23</span>| all rights reserved.
+     </footer>
+  )
+}
+
+export default Footer
